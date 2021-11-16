7744177c199e3338a5cda398ac62d95a0ba78f5b
Abijah Kajabika <migishoabijah@gmail.com>
2021-11-16 05:34:49 +0300

    Updated to expo 43

--- App.js
+++ App.js
@@ -1,62 +1,28 @@
-import { AppLoading, Asset, Font, Icon } from 'expo'
-import React from 'react'
-import { StatusBar, StyleSheet, View } from 'react-native'
-import AppNavigator from './navigation/AppNavigator'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  useFonts,
+  Montserrat_700Bold,
+  Montserrat_600SemiBold,
+  Montserrat_800ExtraBold
+} from '@expo-google-fonts/montserrat';
+import AppLoading from "expo-app-loading";
+import MainTabNavigator from "./navigation/MainTabNavigator";
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  }
 
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      )
-    } else {
-      return (
-        <View style={styles.container}>
-          <StatusBar hidden />
-          <AppNavigator />
-        </View>
-      )
-    }
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    Montserrat_700Bold,
+    Montserrat_600SemiBold,
+    Montserrat_800ExtraBold
+  });
+  if(!fontsLoaded) {
+    return <AppLoading/>
   }
-
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/splash.png'),
-        require('./assets/images/icon.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.MaterialIcons.font,
-        ...Icon.MaterialCommunityIcons.font,
-        ...Icon.FontAwesome.font,
-        ...Icon.Feather.font,
-      }),
-    ])
-  }
-
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error)
-  }
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true })
-  }
+  return(
+      <NavigationContainer>
+        <MainTabNavigator/>
+      </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-})
+export default App;
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,86 +1,50 @@
 import { Icon } from 'expo'
 import React from 'react'
 import {
-  createBottomTabNavigator,
   createStackNavigator,
-} from 'react-navigation'
+} from '@react-navigation/stack'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import TabBarIcon from '../components/TabBarIcon'
 import HomeScreen from '../screens/HomeScreen'
 import MessagesScreen from '../screens/MessagesScreen'
 import ProfileScreen from '../screens/ProfileScreen'
 import TopPicksScreen from '../screens/TopPicksScreen'
 
-const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  {
-    headerMode: 'none',
-  },
-)
+const Bottom = createBottomTabNavigator();
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      Icon={Icon.MaterialCommunityIcons}
-      focused={focused}
-      name="fire"
-    />
-  ),
+const MainTabNavigator = ({}) => {
+  return(
+      <Bottom.Navigator>
+        <Bottom.Screen component={HomeScreen} name="Home" navigationOptions={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ focused }) => (
+                <TabBarIcon
+                    Icon={Icon.MaterialCommunityIcons}
+                    focused={focused}
+                    name="fire"
+                />
+            ),
+        }} />
+          <Bottom.Screen component={TopPicksScreen} name="TopPicks" navigationOptions={{
+              tabBarLabel: 'TopPicks',
+              tabBarIcon: ({ focused }) => (
+              <TabBarIcon Icon={Icon.FontAwesome} focused={focused} name="diamond" />
+              ),
+          }} />
+          <Bottom.Screen component={MessagesScreen} name="Messages" navigationOptions={{
+              tabBarLabel: 'Messages',
+              tabBarIcon: ({ focused }) => (
+                  <TabBarIcon Icon={Icon.FontAwesome} focused={focused} name="commenting-o" />
+              ),
+          }} />
+        <Bottom.Screen component={ProfileScreen} name="Profile" navigationOptions={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({focused}) => (
+              <TabBarIcon Icon={Icon.Feather} focused={focused} name="user"/>
+          ),
+        }} />
+      </Bottom.Navigator>
+  )
 }
 
-const TopPicksStack = createStackNavigator(
-  {
-    TopPicks: TopPicksScreen,
-  },
-  {
-    headerMode: 'none',
-  },
-)
-
-TopPicksStack.navigationOptions = {
-  tabBarLabel: 'TopPicks',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon Icon={Icon.FontAwesome} focused={focused} name="diamond" />
-  ),
-}
-
-const MessagesStack = createStackNavigator(
-  {
-    Messages: MessagesScreen,
-  },
-  {
-    headerMode: 'none',
-  },
-)
-
-MessagesStack.navigationOptions = {
-  tabBarLabel: 'Messages',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon Icon={Icon.FontAwesome} focused={focused} name="commenting-o" />
-  ),
-}
-
-const ProfileStack = createStackNavigator(
-  {
-    Profile: ProfileScreen,
-  },
-  {
-    headerMode: 'none',
-  },
-)
-
-ProfileStack.navigationOptions = {
-  tabBarLabel: 'Profile',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon Icon={Icon.Feather} focused={focused} name="user" />
-  ),
-}
-
-export default createBottomTabNavigator({
-  HomeStack,
-  TopPicksStack,
-  MessagesStack,
-  ProfileStack,
-})
+export default MainTabNavigator
