0e6fd4ccc649a60deef66216fc7438a659c91582
Abijah Migisho <Abijah.Migisho@noveogroup.com>
2021-12-15 15:29:14 +0300

    .

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,47 +1,69 @@
-import { Icon } from 'expo'
+
 import React from 'react'
-import {
-  createStackNavigator,
-} from '@react-navigation/stack'
+
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
 import TabBarIcon from '../components/TabBarIcon'
 import HomeScreen from '../screens/HomeScreen'
 import MessagesScreen from '../screens/MessagesScreen'
 import ProfileScreen from '../screens/ProfileScreen'
 import TopPicksScreen from '../screens/TopPicksScreen'
+import Icon from '@expo/vector-icons/Ionicons';
+import Colors from "../constants/Colors";
 
 const Bottom = createBottomTabNavigator();
 
+
+
 const MainTabNavigator = ({}) => {
   return(
-      <Bottom.Navigator>
+      <Bottom.Navigator tabBarOptions={{
+          activeTintColor:'#f2404c',
+          inactiveTintColor:'#000119',
+          style:{
+              justifyContent:'center',
+              backgroundColor:'#FFF',
+              elevation:2
+          }
+      }}>
         <Bottom.Screen component={HomeScreen} name="Home" navigationOptions={{
             tabBarLabel: 'Home',
             tabBarIcon: ({ focused }) => (
-                <TabBarIcon
-                    Icon={Icon.MaterialCommunityIcons}
-                    focused={focused}
+                <Icon
                     name="fire"
+                    size={26}
+                    style={{ marginBottom: -3 }}
+                    color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
                 />
             ),
         }} />
           <Bottom.Screen component={TopPicksScreen} name="TopPicks" navigationOptions={{
               tabBarLabel: 'TopPicks',
               tabBarIcon: ({ focused }) => (
-              <TabBarIcon Icon={Icon.FontAwesome} focused={focused} name="diamond" />
+                  <Icon
+                      name="diamond"
+                      size={26}
+                      style={{ marginBottom: -3 }}
+                      color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+                  />
               ),
           }} />
           <Bottom.Screen component={MessagesScreen} name="Messages" navigationOptions={{
               tabBarLabel: 'Messages',
               tabBarIcon: ({ focused }) => (
-                  <TabBarIcon Icon={Icon.FontAwesome} focused={focused} name="commenting-o" />
+                  <Icon
+                      name="commenting-o"
+                      size={26}
+                      style={{ marginBottom: -3 }}
+                      color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+                  />
               ),
           }} />
         <Bottom.Screen component={ProfileScreen} name="Profile" navigationOptions={{
           tabBarLabel: 'Profile',
-          tabBarIcon: ({focused}) => (
-              <TabBarIcon Icon={Icon.Feather} focused={focused} name="user"/>
-          ),
+            tabBarIcon:({color,size})=>(
+                <Icon name='ios-person' color={color} size={30}/>
+            )
         }} />
       </Bottom.Navigator>
   )
